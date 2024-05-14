b2aa9503ce1fc0178b673e520f08316293770d6c
coderwhytop <1795691637@qq.com>
2024-05-14 14:46:56 +0800

    精简vue的todo

--- site/vue3/src/pages/baseUse/index.vue
+++ site/vue3/src/pages/baseUse/index.vue
@@ -1,37 +1,13 @@
 <script lang="ts" setup>
 import Todo from "@/components/Todo/Index.vue";
 import { ref } from "vue";
-import { INF_LIST_ITEM, TYPE_LIST } from "@/types/todoList.ts";
-import { getList, delItem, addItem, changeItem, DelMultiple } from "@/utils/todo.ts";
-import { $Notification } from "@/utils/toast.ts";
-
+import { getList, delItem, addItem, TYPE_LIST} from "@/utils/todo.ts";
 const todoList = ref(getList() as TYPE_LIST);
 const del = function (content: string) {
   delItem(todoList.value, content)
-    .then((r:string)=> $Notification({content: r}))
-    .catch((e:string) => $Notification({content:e, type: 'error'}))
-};
-
-const change = function (index:number, key: keyof INF_LIST_ITEM, value: string | boolean) {
-  changeItem(todoList.value, index, key,value)
-    .then((r:string)=>  {
-      key !== 'input' && $Notification({content: r})
-      console.log(key,'key');
-    })
-    .catch((e:string) => $Notification({content:e, type: 'error'}))
 };
 const add = function (content: string) {
   addItem(todoList.value, content)
-    .then((r:string)=> $Notification({content: r}))
-    .catch((e:string) => $Notification({content:e, type: 'error'}))
 };
-const multipleDel = function(ids: Array<string>){
-  DelMultiple(todoList.value, ids)
-    .then((r:string)=> $Notification({content: r}))
-    .catch((e:string) => $Notification({content:e, type: 'error'}))
-}
 </script>
-
-<template>
-  <Todo :list="todoList" @add="add" @change="change" @del="del" @multipleDel="multipleDel" />
-</template>
+<template><Todo :list="todoList" @add="add"  @del="del" /></template>
