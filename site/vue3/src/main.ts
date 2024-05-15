176b638fe45f1c081df41ac3bc8985b799495fdc
xiaoyu <xiaoyu@sulg.top>
2024-05-15 20:57:34 +0800

    ✨ 全局注册codeDemo组件

--- site/vue3/src/main.ts
+++ site/vue3/src/main.ts
@@ -8,6 +8,7 @@ import "@arco-design/web-vue/es/message/style/css.js";
 import "@arco-design/web-vue/es/notification/style/css.js";
 import "@arco-design/web-vue/es/modal/style/css.js";
 import hljsVuePlugin from "@highlightjs/vue-plugin";
+import CodeDemo from "@/components/CodeDemo/index.vue";
 import "./app.less";
 
 const pinia = createPinia();
@@ -16,4 +17,5 @@ const app = createApp(App);
 Message._context = app._context;
 Notification._context = app._context;
 Modal._context = app._context;
+app.component("code-demo", CodeDemo); // vue 可以在全局CodeDemo注册，react中是没有的
 app.use(router).use(pinia).use(hljsVuePlugin).mount("#vue-app");
