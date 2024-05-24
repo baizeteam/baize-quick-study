27ae9ad197ae63de9e03a35a883837682e826239
xiaoyu <xiaoyu@sulg.top>
2024-05-24 22:56:56 +0800

    ✨ 微前端添加调试配置

--- site/main/src/main.tsx
+++ site/main/src/main.tsx
@@ -3,6 +3,9 @@ import App from "./app";
 import "./index.less";
 import microApp from "@micro-zoe/micro-app";
 
-microApp.start();
+microApp.start({
+  inline: import.meta.env.DEV,
+  "keep-alive": true, // 好像没有效果
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
