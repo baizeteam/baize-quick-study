22037534c0a76f08b10542b665c089bfbc141ea4
xiaoyu <xiaoyu@sulg.top>
2024-06-26 22:08:04 +0800

    ✨ 调整mock数据前缀

--- site/react/src/mock/index.ts
+++ site/react/src/mock/index.ts
@@ -1,23 +1,22 @@
-import type { MockMethod } from 'vite-plugin-mock'
+import type { MockMethod } from "vite-plugin-mock";
 
 export default [
-    {
-        url: '/api/mock/project/info', // 模拟获取当前项目信息的链接
-        method: 'get',
-        timeout: 500,
-        statusCode: 200, // 返回状态码
-        response() {
-            return {
-                code: 200,
-                message: '请求成功！',
-                data: {
-                    name: 'baize-team',
-                    type: '来自React中模拟的数据',
-                    projectName: 'baize-quick-study',
-                    desc: '为各位有React或者Vue基础的同学，快速学习未掌握的React或者Vue框架'
-                }
-            }
-          
-        }
-    }
-] as MockMethod[]
+  {
+    url: "/apiReact/mock/project/info", // 模拟获取当前项目信息的链接
+    method: "get",
+    timeout: 500,
+    statusCode: 200, // 返回状态码
+    response() {
+      return {
+        code: 200,
+        message: "请求成功！",
+        data: {
+          name: "baize-team",
+          type: "来自React中模拟的数据",
+          projectName: "baize-quick-study",
+          desc: "为各位有React或者Vue基础的同学，快速学习未掌握的React或者Vue框架",
+        },
+      };
+    },
+  },
+] as MockMethod[];
--- site/react/src/utils/request.ts
+++ site/react/src/utils/request.ts
@@ -1,30 +1,35 @@
-import axios from 'axios'
-import { AxiosInstance, InternalAxiosRequestConfig, AxiosResponse, AxiosError } from 'axios'
+import axios from "axios";
+import { AxiosInstance, InternalAxiosRequestConfig, AxiosResponse, AxiosError } from "axios";
 
 const request: AxiosInstance = axios.create({
-    baseURL: '/api/',
-    timeout: 2000,
-    headers: {
-        'Content-Type': 'aapplication/json;charset=UTF-8'
-    }
+  baseURL: "/apiReact/",
+  timeout: 2000,
+  headers: {
+    "Content-Type": "aapplication/json;charset=UTF-8",
+  },
+});
 
-})
+request.interceptors.request.use(
+  (config: InternalAxiosRequestConfig) => {
+    return config;
+  },
+  (error: AxiosError) => {
+    return Promise.reject(error);
+  },
+);
 
-request.interceptors.request.use((config: InternalAxiosRequestConfig) => {
-    return config
-}, (error: AxiosError) => {
-    return Promise.reject(error)
-})
-
-request.interceptors.response.use((response: AxiosResponse) => {
-    const code = response.data.code
+request.interceptors.response.use(
+  (response: AxiosResponse) => {
+    const code = response.data.code;
     if (code !== 200) {
-       return Promise.reject(response)
+      return Promise.reject(response);
     } else {
-       return Promise.resolve(response.data)
+      return Promise.resolve(response.data);
     }
-}, (error: AxiosError) => {
-    return Promise.reject(error)
-})
+  },
+  (error: AxiosError) => {
+    return Promise.reject(error);
+  },
+);
 
-export default request
+export default request;
